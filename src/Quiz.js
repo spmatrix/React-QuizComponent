ccc3a71e6e1d3ca1e8f68acb206024a7dddc89df
spmatrix <spmatrix@live.com>
2018-11-21 17:16:59 -0600

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {Component} from 'react'
 let quizData = require('./quiz_data.json')
 
-class Quiz extends React.Component {
+class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = { quiz_position: 1 }

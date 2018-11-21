d62d25ec3c32dcd854b726dd219c6fc983c14067
spmatrix <spmatrix@live.com>
2018-11-21 17:50:00 -0600

    completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component
 {  
@@ -9,10 +10,9 @@ class QuizQuestion extends Component
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className='buttons'>
-                    <ul>
-                      <li>
+                    <ul>                      
                         {this.props.quiz_question.answer_options[0]}
-                      </li>
+                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
                     </ul>
                 </section>
             </main>
